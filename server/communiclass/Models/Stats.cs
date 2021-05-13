757b9a90c16f3725e43939e093cd748c0e0ac0a7
Yael Mualem <yael.mualem@mail.huji.ac.il>
2021-05-13 15:34:21 +0300

    adding server side

---
+++ server/communiclass/Models/Stats.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace communiclass.Models
+{
+    public class Stats
+    {
+        public DateTime Date;
+
+        public double Avg;
+    }
+}
---
+++ server/communiclass/Services/Concrete/StatisticManager.cs
@@ -0,0 +1,32 @@
+﻿using communiclass.Models;
+using communiclass.Services.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace communiclass.Services.Concrete
+{
+    public class StatisticManager : IStatisticManager
+    {
+        public async Task UpdateUserGrade(int userId, int pin, int grade)
+        {
+            return;
+        }
+
+        public async Task<double> GetCurrentAvgGrade(int pin)
+        {
+            return Convert.ToDouble(1);
+        }
+
+        public async Task<Stats[]> GetStats(int pin)
+        {
+            var x = new Stats
+            {
+                Date = DateTime.Now,
+                Avg = Convert.ToDouble(1)
+            };
+            return new Stats[] { x };          
+        }
+    }
+}
