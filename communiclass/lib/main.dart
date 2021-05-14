17e58aa8d93dc40f8f41c48e2335cf74366f556d
ditah22 <itah.dor@gmail.com>
2021-05-14 15:03:10 +0300

    Setup connection to firebase

--- communiclass/lib/main.dart
+++ communiclass/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:communiclass/services/auth.dart';
 import 'package:flutter/material.dart';
 import 'package:communiclass/pages/home.dart';
 import 'package:communiclass/pages/room.dart';
@@ -9,7 +10,7 @@ void main() {
   runApp(MaterialApp(
     initialRoute: '/home',
     routes: {
-      '/home':(context) => Home(),
+      '/home': (context) => Home(),
       '/create_room': (context) => OpenRoom(),
       '/room': (context) => Room(),
       '/join_room': (context) => JoinRoom(),
@@ -17,3 +18,24 @@ void main() {
     },
   ));
 }
+// void main() => runApp(MyApp());
+//
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return StreamProvider<User>.value(
+//       value: AuthService().user,
+//       child: MaterialApp(
+//         // initialRoute: '/home',
+//         // routes: {
+//         //   '/home': (context) => Home(),
+//         //   '/create_room': (context) => OpenRoom(),
+//         //   '/room': (context) => Room(),
+//         //   '/join_room': (context) => JoinRoom(),
+//         //   '/teacher_room': (context) => TeacherRoom(),
+//         // },
+//         home: Home(),
+//       ),
+//     );
+//   }
+// }
